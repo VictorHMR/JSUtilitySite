5836abbcd26b31883938a2aa99f4f554834578ac
Victor Hugo Machado Rios <75526228+VictorHMR@users.noreply.github.com>
2022-04-09 18:56:38 -0300

    Mensagem

---
+++ js/encode.js
@@ -0,0 +1,23 @@
+let Sha = $("#BSha1");
+let Md5 = $("#BMD5")
+let input = $("#input").val();
+let output = $('#output');
+
+function Convert_Sha1() {
+    var encoded = sha1(input); 
+    output.val(encoded);
+}
+function Convert_Md5() {
+    var encoded = md5(input); 
+    output.val(encoded);
+}
+
+
+
+Sha.on("click", ()=>{
+    Convert_Sha1();
+});
+
+Md5.on("click", ()=>{
+    Convert_Md5();
+})
