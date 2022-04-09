2c6775785967dee31e1f45f299c7b1da9285d0fc
Victor Hugo Machado Rios <75526228+VictorHMR@users.noreply.github.com>
2022-04-09 19:10:37 -0300

    Hash Encoder

--- js/encode.js
+++ js/encode.js
@@ -1,23 +1,23 @@
 let Sha = $("#BSha1");
-let Md5 = $("#BMD5")
-let input = $("#input").val();
-let output = $('#output');
+let Md5 = $("#BMD5");
 
 function Convert_Sha1() {
-    var encoded = sha1(input); 
-    output.val(encoded);
+  let input = $("#input").val();
+  let output = $("#output");
+  var encoded = sha1(input);
+  output.val(encoded);
 }
 function Convert_Md5() {
-    var encoded = md5(input); 
-    output.val(encoded);
+  let input = $("#input").val();
+  let output = $("#output");
+  var encoded = md5(input);
+  output.val(encoded);
 }
 
-
-
-Sha.on("click", ()=>{
-    Convert_Sha1();
+Sha.on("click", () => {
+  Convert_Sha1();
 });
 
-Md5.on("click", ()=>{
-    Convert_Md5();
-})
+Md5.on("click", () => {
+  Convert_Md5();
+});
